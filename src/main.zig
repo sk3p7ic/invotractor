470a5183796a9e83753171e828c4343a22adbcdb
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2024-04-20 13:47:34 -0500

    feat: simple zap 'Hello, world!' server
    Followed the brief tutorial on the zap GH repo to get a simple server
    up and running to confirm that the environment works.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,29 @@
 const std = @import("std");
+const zap = @import("zap");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+fn on_req(r: zap.Request) void {
+    if (r.path) |path| {
+        std.debug.print("PATH: {s}\n", .{path});
+    }
+    if (r.query) |query| {
+        std.debug.print("QUERY: {s}\n", .{query});
+    }
+    const markup =
+        \\<html>
+        \\  <head>
+        \\      <title>Invotractor v0.0.1</title>
+        \\  </head>
+        \\  <body>
+        \\      <h1>Hello, world!</h1>
+        \\  </body>
+        \\</html>
+    ;
+    r.sendBody(markup) catch return;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    var listener = zap.HttpListener.init(.{ .port = 3000, .on_request = on_req, .log = true });
+    try listener.listen();
+    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+    zap.start(.{ .threads = 2, .workers = 2 });
 }
